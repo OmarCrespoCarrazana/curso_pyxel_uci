b79918b8591b5f5196193e6eda892df3c0cc51b1
ludRey <twipzcuba@gmail.com>
2025-03-01 16:11:22 -0500

    fix(manifest): add mass_mailing dependence

--- childcare_management/__manifest__.py
+++ childcare_management/__manifest__.py
@@ -9,9 +9,10 @@
     "category": "Services",
     "version": "0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base", "website", "hr", "product", "stock", "crm", "account", "mail"],
+    "depends": ["base","mass_mailing", "website", "hr", "product", "stock", "crm", "account", "mail"],
     # always loaded
     "data": [
+        'security/daycare_security.xml',
         'security/ir.model.access.csv',
         "views/child_views.xml",
         "views/classroom_views.xml",
