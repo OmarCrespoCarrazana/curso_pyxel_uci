5a4687dafb6880b2472c7eb8be1cd859859a6b83
Enrique Ferreiro <ztazhorde@gmail.com>
2025-03-02 00:40:18 -0500

    change(__manifest__) update data and assets

--- childcare_management/__manifest__.py
+++ childcare_management/__manifest__.py
@@ -9,20 +9,35 @@
     "category": "Services",
     "version": "0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base", "website", "hr", "product", "stock", "crm", "account", "mail"],
-    # always loaded
+    "depends": ["base", "website", "hr", "product", "stock", "crm", "account", "mail", "portal"],
     "data": [
-        # 'security/ir.model.access.csv',
-        "views/views.xml",
-        "views/templates.xml",
-    ],
-    # only loaded in demonstration mode
-    "demo": [
-        "demo/demo.xml",
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'templates/base_layout.xml',
+        'templates/navbar.xml',
+        'templates/footer.xml',
+        'templates/breadcrumbs.xml',
+        'templates/login_base.xml',
+        'templates/login.xml',  # Asegúrate de que esté aquí y sin espacios
+        'templates/dashboard.xml',
+        'templates/children.xml',
+        'templates/medical_records.xml',
+        'templates/components/modal_base.xml',
+        'templates/components/service_form.xml',
+        'templates/services.xml',
+        'templates/reusable_table.xml',
+        'templates/invoices.xml',
     ],
     "assets": {
-        "web.assets_backend": [],
-        "web.assets_frontend": [],
+        "web.assets_backend": [
+        ],
+        "web.assets_frontend": [   
+            ("include", "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"),
+            "/childcare_management/static/src/css/index.css",
+        ],
+        "web.assets_common": [
+            ("include", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"),
+        ]
     },
     "application": True,
     "installable": True,
