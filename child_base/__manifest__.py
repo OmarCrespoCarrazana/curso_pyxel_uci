a5247517bcf26a8ba171c4f4c7d8b15a68b0ed72
ludRey <twipzcuba@gmail.com>
2025-03-01 23:47:07 -0500

    Add child base module allowing extensions and more modularity

---
+++ child_base/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Childcare Base",
+    "summary": """ Childcare Base Module """,
+    "description": """
+    """,
+    "author": "Pyxel SLR Odoo`s Trainees  Team",
+    "website": "",
+    "category": "Services",
+    "version": "0.1",
+    # any module necessary for this one to work correctly
+    "depends": ["base","web","mass_mailing", "website", "hr", "product", "stock", "crm", "account", "mail"],
+    # always loaded
+    "data": [
+     #   'security/ir.model.access.csv',
+     #   "views/child_views.xml",
+        'data/cron_jobs.xml',
+       
+    ],
+    # only loaded in demonstration mode
+    "demo": [
+    #    "demo/demo.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [],
+        "web.assets_frontend": [],
+    },
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
