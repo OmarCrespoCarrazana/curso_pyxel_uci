a60e3191ddceeb6d6099bcaba9cc1fe83b1fbb7e
José Ramón <jose.1999.rfg@gmail.com>
2025-03-02 00:20:26 -0500

    fix(contracts): correct errors in customer.contract model

--- customer_contract/__manifest__.py
+++ customer_contract/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Contract invoice managment',
     'author': 'Ing.José Ramón Fidalgo García, M.Sc.Yadira Ramírez Rodríguez',
     'category': 'Services',
-    'depends': ['base', 'calendar', 'website', 'mail', 'account','payment', 'crm'],
+    'depends': ['base', 'calendar', 'website', 'mail', 'account','payment', 'crm', 'childcare_management'],
     'data': [
         'security/ir.model.access.csv',
         'data/sequences.xml',
--- customer_contract/models/customer_contract.py
+++ customer_contract/models/customer_contract.py
@@ -1,68 +1,43 @@
 from odoo import models, fields, api
 from datetime import date
 from odoo.exceptions import ValidationError
-
+from dateutil.relativedelta import relativedelta
+from calendar import monthrange
 
 class CustomerContract(models.Model):
     _name = 'customer.contract'
     _description = 'Clients contracts'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string='Code', default=lambda self: self.env['ir.sequence'].next_by_code('customer.contract'), readonly=True)
-    start_date = fields.Date(string='Start of contract', required=True)
-    end_date = fields.Date(string='End of contract', required=True)
-
+    name = fields.Char(string='Code', default=lambda self: self.env['ir.sequence'].next_by_code('customer.contract'), readonly=True )
+    start_date = fields.Date(string='Start of contract', required=True, readonly=True, default=fields.Date.today)
+    end_date = fields.Date(string='End of contract', required=True, readonly=True)
     partner_id = fields.Many2one('res.partner', string='Tutor', required=True)
-
     state = fields.Selection(selection=[
-        ('in_process', 'In process'),
         ('valid', 'Valid'),
         ('expired', 'Expired'),
-        ('finalized','Finalized')
-    ], string='Contract state', default='in_process')
-
+    ], string='Contract state', default='valid')
+    invoice_id = fields.Many2many('account.move', string='Invoice', readonly=True, required=False)
     
-    total_payment_amount = fields.Float(string='Total Debt', required=True)
-
-
-    """
-    Invoice related to contract payment
-    """
-    invoice_id = fields.Many2one('account.move', string='Invoice', readonly=True, required=False)
-
-    """
-    supplements = fields.Many2one()
-    child_id = field.Many2one()
-    """
-
-    def unlink(self):
+    payment_day = fields.Integer(string="Day to issue the invoice.", required=True)
+    
+    @api.constrains("payment_day")
+    def _check_payment_day_range(self):
         for record in self:
-            if record.invoice_id and record.invoice_id.state == 'posted':
-                raise ValidationError("You cannot delete this contract because the invoice is already approved.")
-            elif record.invoice_id:
-                record.invoice_id.unlink() 
-        return super(CustomerContract, self).unlink()
+            if record.payment_day < 1 or record.payment_day > 24:
+                raise ValidationError("The number of hours must be between 1 and 24.")
 
-    def write(self, vals):
-        res = super(CustomerContract, self).write(vals)
-        if 'state' in vals and vals['state'] in ['expired', 'finalized']:
-                income_account = self.env['account.account'].search([('account_type', '=', 'income')], limit=1)
-                invoice_vals = {
-                    'move_type': 'out_invoice',
-                    'partner_id': res.partner_id.id,
-                    'invoice_date': fields.Date.today(),
-                    'invoice_payment_term_id': False,  
-                    'invoice_line_ids': [(0, 0, {
-                        'name': f'Contract Payment - {res.name}',
-                        'quantity': 1,
-                        'price_unit': res.total_payment_amount,
-                        'account_id': income_account.id if income_account else False
-                    })]
-                }
-                invoice = self.env['account.move'].create(invoice_vals)
-                res.invoice_id = invoice.id
-        return res
-
+    @api.model
+    def create(self, vals):
+        if "start_date" not in vals:
+            vals["start_date"] = fields.Date.today()
+        if "end_date" not in vals:
+            vals["end_date"] = vals["start_date"] + relativedelta(months=6)
+        contract = super(CustomerContract, self).create(vals)  
+            
+        return contract
+         
+    
     
     def update_customer_tatus(self):
         today = date.today()
@@ -71,11 +46,40 @@ class CustomerContract(models.Model):
             if contract.end_date and contract.end_date < today:
                 contract.state = 'expired'
                 
-                self.send_expiration_email(contract)
+                #self.send_expiration_email(contract)
+                
+        
+        contracts_to_invoice = self.search([
+            ('payment_day', '=', today.day)
+        ])
+        for contract in contracts_to_invoice:
+            end_date_last_day = date(contract.end_date.year, contract.end_date.month, monthrange(contract.end_date.year, contract.end_date.month)[1]) + relativedelta(months=1)
+            if  today <= end_date_last_day:
+                self.generate_invoice_if_due(self, contract)
 
-    
+
+    def generate_invoice_if_due(self, contract):
+        """
+        incluir los insumos y cosos por mora 
+        """
+        income_account = self.env["account.account"].search([("account_type", "=", "income")], limit=1)
+        invoice_vals = {
+            "move_type": "out_invoice",
+            "partner_id": contract.partner_id.id,
+            "invoice_date": fields.Date.today(),
+            "invoice_payment_term_id": False,
+            "invoice_line_ids": [(0, 0, {
+                "name": f"Contract Payment - {contract.name}",
+                "quantity": 1,
+                "price_unit": 500,
+                "account_id": income_account.id if income_account else False,
+            })],
+        }
+        invoice = self.env["account.move"].create(invoice_vals)
+        contract.invoice_id = [(4, invoice.id)]
+
     def send_expiration_email(self, contract):
-        template = self.env.ref('contract_invoice_management.email_template_contract_expiration')
+        template = self.env.ref('customer_contract.email_template_contract_expiration')
         if template:
             email_values = {
                 'email_to': contract.partner_id.email,  
@@ -107,12 +111,6 @@ class CustomerContract(models.Model):
                 'res_id': new_contract.id,
                 'target': 'current',
             }
-    def action_valid_contract(self):
-        for contract in self:
-            if contract.state != 'in_process':
-                raise ValidationError("Only contracts in process can be validated.")
-
-            contract.state = 'valid'
 
 
 
