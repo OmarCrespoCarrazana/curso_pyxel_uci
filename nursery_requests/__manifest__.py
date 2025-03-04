a41c5f784fc171186157ee77ab8efa69e60e1aa4
LeudiX <leudix.rafael@gmail.com>
2025-03-03 20:29:09 -0500

    feat(manifest): Updated manifest

--- nursery_requests/__manifest__.py
+++ nursery_requests/__manifest__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Nursery Requests",
-    "summary": """ Childcare Management Nursery Request Module """,
-    "description":
-    """ 
-    This module process all the requests made 
-    by the nursery staff over Medical Supplies and Child Medication stock products
+    "summary": """ Childcare Management: Nursing Medical Request Module""",
+    "description": """
+    This module process all the requests made by the nursery staff over Medical Supplies and Child Medication stock products
     """,
     "author": "LeudiX (Odoo Junior Developer)",
     "website": "",
@@ -13,20 +11,22 @@
     "version": "1.0",
     # any module necessary for this one to work correctly
     "depends": [
-        "base",     # Core module (Users, Security, Models)
-        "hr",       # Employees (Nurses & Storekeepers)
+        "base",  # Core module (Users, Security, Models)
+        "hr",  # Employees (Nurses & Storekeepers)
         "product",  # Product categories and minimal stock
         "purchase",  # Supply Procurement
-        "stock",    # Inventory & Warehouse
-        "mail"],    # Messaging & Notifications for system users
+        "stock",  # Inventory & Warehouse
+        "mail",
+    ],  # Messaging & Notifications for system users
     # always loaded
-    "data": [
+    "data": [  # Order matters
         "security/security.xml",
         "security/ir.model.access.csv",
         "data/sequences.xml",
-        "views/nursery_request_menu_views.xml",
-        "views/nursery_request_views.xml",
         "views/actions.xml",
+        "views/nursery_requests_views.xml",
+        "views/nursery_request_line_views.xml",
+        "views/nursery_requests_menu_views.xml",
     ],
     "application": True,
     "installable": True,
